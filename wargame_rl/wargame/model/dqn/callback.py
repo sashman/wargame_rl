3a03478bfe8bea1dce1aba1d57ee9271adf3178c
Nathanael Perraudin <nathanael.perraudin@synthesia.io>
2025-07-26 23:15:19 +0200

    updat

--- wargame_rl/wargame/model/dqn/callback.py
+++ wargame_rl/wargame/model/dqn/callback.py
@@ -5,9 +5,9 @@ def get_checkpoint_callback(name: str):
     # Create checkpoint callback
     checkpoint_callback = ModelCheckpoint(
         dirpath=f"./checkpoints/{name}",
-        filename="dqn-{epoch:03d}-{success_rate:.3f}-{mean_episode_reward:.3f}",
+        filename="dqn-{epoch:03d}-{mean_episode_reward:.3f}",
         save_top_k=3,
-        monitor="success_rate",
+        monitor="mean_episode_reward",
         mode="max",
         save_last=True,
     )
