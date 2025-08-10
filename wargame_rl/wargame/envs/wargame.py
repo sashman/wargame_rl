5878455a706d278fd2946faf603d1d33ac6e4d3c
Nathanael Perraudin <nathanael.perraudin@synthesia.io>
2025-08-10 12:21:09 +0200

    update

--- wargame_rl/wargame/envs/wargame.py
+++ wargame_rl/wargame/envs/wargame.py
@@ -72,7 +72,7 @@ class WargameEnv(gym.Env):
     def __init__(self, config: WargameEnvConfig):
         self.size = config.size  # The size of the square grid
         self.window_size = 1024  # The size of the PyGame window
-
+        self.config = config
         self.observation_space = spaces.Dict(
             {
                 "current_turn": spaces.Discrete(1),
@@ -137,7 +137,7 @@ class WargameEnv(gym.Env):
         # List to hold objectives
         self.objectives = [
             WargameObjective(location=np.zeros(2, dtype=int))
-            for _ in range(config.number_of_wargame_models)
+            for _ in range(config.number_of_objectives)
         ]
 
         # Set the deployment zone for the agent, area left third of the grid
