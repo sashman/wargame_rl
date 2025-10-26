0025adfd7990a45154ef808c5cebc9cc1ac2c739
Sash <sashman90@gmail.com>
2025-10-26 17:38:54 +0000

    env test type fixes

--- wargame_rl/wargame/envs/env_test.py
+++ wargame_rl/wargame/envs/env_test.py
@@ -5,18 +5,22 @@ from wargame_rl.wargame.envs.types import WargameEnvAction, WargameEnvConfig
 class EnvTest:
     # Set up the environment and and run N numbe of acitons
 
-    def __init__(self, size, render_mode="human"):
+    def __init__(self, size: int, render_mode: str | None = "human") -> None:
         from wargame_rl.wargame.envs.wargame import WargameEnv
 
         self.env = WargameEnv(
             WargameEnvConfig(
-                size=size, render_mode=render_mode, number_of_wargame_models=3
+                size=size,
+                render_mode=render_mode,
+                number_of_wargame_models=3,
+                number_of_objectives=2,
+                deployment_zone=(0, 0, size // 3, size),
             ),
             renderer=HumanRender(),
         )
         self.env.reset()
 
-    def run_actions(self, num_actions=5):
+    def run_actions(self, num_actions: int = 5) -> None:
         for _ in range(num_actions):
             action = self.env.action_space.sample()
             observation, reward, terminated, truncated, info = self.env.step(
