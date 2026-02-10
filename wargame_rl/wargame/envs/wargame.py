8138d04c4f72d5e885a1ac1ab06f219e214d0f76
Sash <sashman90@gmail.com>
2026-02-10 21:04:51 +0000

    Using exclusive ranges

--- wargame_rl/wargame/envs/wargame.py
+++ wargame_rl/wargame/envs/wargame.py
@@ -174,12 +174,12 @@ class WargameEnv(gym.Env):
         )
 
     def is_within_deployment_zone(self, location: np.ndarray) -> bool:
-        """True if this location is within the deployment zone."""
+        """True if this location is within the deployment zone (bounds [x0, x2), [y0, y3))."""
         return bool(
             location[0] >= self.deployment_zone[0]
-            and location[0] <= self.deployment_zone[2]
+            and location[0] < self.deployment_zone[2]
             and location[1] >= self.deployment_zone[1]
-            and location[1] <= self.deployment_zone[3]
+            and location[1] < self.deployment_zone[3]
         )
 
     def reset(
